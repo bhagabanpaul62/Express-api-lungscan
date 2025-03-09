6c1f52f97a26a06991597a42eb8b8c158edc46d1
bhagabanpaul62 <160720125+bhagabanpaul62@users.noreply.github.com>
2025-03-09 21:41:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+
+const upload = multer({ dest: "uploads/" });
+
+app.post("/predict", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const imagePath = path.resolve(req.file.path);
+    const formData = new FormData();
+    formData.append("file", fs.createReadStream(imagePath));
+
+    const response = await axios.post(
+      "http://localhost:5000/predict",
+      formData,
+      {
+        headers: formData.getHeaders(),
+      }
+    );
+
+    // Cleanup: Delete the uploaded file after processing
+    fs.unlinkSync(imagePath);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error processing image:", error.message);
+    res.status(500).json({
+      error: error.response ? error.response.data : "Internal Server Error",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
